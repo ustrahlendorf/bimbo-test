af62ff2a1ca56644c1e31145cd77436f2e80b5f4
Uwe Strahlendorf <ustrahlendorf@tecracer.de>
2022-01-21 20:18:49 +0100

    restructuring to deploy Vpc only

--- VpcOnlyNoIGW/src/variables.tf
+++ VpcOnlyNoIGW/src/variables.tf
@@ -3,13 +3,3 @@ variable "instance_name" {
   type        = string
   default     = "TF-ExampleInstance"
 }
-variable "appServer_instance_type" {
-  description = "type for the EC2 instance"
-  type        = string
-  default     = "t2.micro"
-}
-variable "ami_amznLinux2" {
-  description = "Amazon Linux 2 AMI (HVM) - Kernel 5.10, SSD Volume Type; eu-central-1"
-  type        = string
-  default     = "ami-05cafdf7c9f772ad2"
-}
