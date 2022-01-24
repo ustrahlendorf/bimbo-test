01d08818c3160f56cb6088153f86e72bf3475c1e
Uwe Strahlendorf <ustrahlendorf@tecracer.de>
2022-01-24 16:21:14 +0100

    add attributes in vpc.tf

--- VpcOnlyNoIGW/src/vpc.tf
+++ VpcOnlyNoIGW/src/vpc.tf
@@ -6,16 +6,26 @@ module "vpc" {
   cidr = "10.10.0.0/16"
 
   azs             = ["${local.region}a", "${local.region}b"]
-  private_subnets = ["10.10.1.0/24", "10.10.2.0/24"]
-  public_subnets  = ["10.10.101.0/24", "10.10.102.0/24"]
+  public_subnets = ["10.10.1.0/24", "10.10.2.0/24"]
+  private_subnets  = ["10.10.101.0/24", "10.10.102.0/24"]
 
-  public_subnet_suffix  = "privSub"
-  private_subnet_suffix = "pubSub"
+  public_subnet_suffix  = "pubSub"
+  private_subnet_suffix = "privSub"
 
-  default_security_group_name = "tf-default"
+  private_subnet_tags = { Name = "privSub-${module.vpc.name}" }
+  public_subnet_tags = { Name = "pubSub-${module.vpc.name}" }
 
-  enable_nat_gateway = false
-  single_nat_gateway = false
+  private_route_table_tags = { Name = "privRT-${module.vpc.name}" }
+  public_route_table_tags = { Name = "pubRT-${module.vpc.name}" }
+
+  manage_default_route_table = true
+  default_route_table_tags = { Name = "default-${module.vpc.name}" }
+  
+  manage_default_security_group = true
+  default_security_group_tags   = { Name = "default-${module.vpc.name}" }
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
 
   enable_vpn_gateway = false
 
